fde6049129da47afa193935c5019eab99ed96866
meeps123 <22002891+meeps123@users.noreply.github.com>
2022-12-08 14:58:50 +0800

    Working on NLvl

---
+++ analysis.py
@@ -0,0 +1,82 @@
+import numpy as np
+import regex
+import editdistance
+
+# ---------------------------------------------------------------------------- #
+#                            Description of Outputs                            #
+# ---------------------------------------------------------------------------- #
+'''
+    hsp:                    The desired Highest Standard Passed Label
+    criteria_passed:        Whether the HSP Classification criteria was PASSED or FAILED
+    specific_doc_class:     The specific educational qualification which the submitted document represents
+    remarks:                Additional Remarks
+'''
+
+def word2num(word):
+    if regex.search(r'[a-z]+', word) is None:
+        # there are no letters in there
+        return None
+    indices = np.full((11), np.inf)
+    for i, reference in enumerate(['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']):
+        indices[i] = editdistance.eval(word, reference)
+    return np.argmin(indices)
+
+# ---------------------------------------------------------------------------- #
+#                          Analyze N-Level Certificate                         #
+# ---------------------------------------------------------------------------- #
+
+def nlvl(full_ocr_result, first_page_merged_text):
+    output = {
+        'hsp': 'O-Level & Below',
+        'criteria_passed': '',
+        'specific_doc_class': '',
+        'remarks': ''
+    }
+
+    # Find out whether this person is N(A) or N(T)
+    specific_doc_class_pattern = r'normal\s*\((\w+)\)\s*level'
+    specific_doc_class_search = regex.search(specific_doc_class_pattern, first_page_merged_text)
+    subtype = specific_doc_class_search.group(1)
+    if subtype == 'technical':
+        output['specific_doc_class'] = 'Normal (Technical)'
+    elif subtype == 'academic':
+        output['specific_doc_class'] = 'Normal (Academic)'
+
+    # Find out whether this person mistakenly submitted the RESULT SLIP instead of the correct N-Level certificate
+    result_slip_search = regex.search(r'(result\s*slip){e<=1}', first_page_merged_text)
+    if result_slip_search is None:
+        # Confirm that it is a N-Level certificate
+        if regex.search(r'(this\s*certifies\s*that){e<=3}', first_page_merged_text) is not None:
+            # Yes, it is a N-Level certificate.
+            hsp_criteria_pattern_cert = r'(graded\s*d\s*or\sbetter\:\s*(\w+)){e<=3}'
+            hsp_criteria_search_cert = regex.search(hsp_criteria_pattern_cert, first_page_merged_text)
+            num_pass_subjects_cert = word2num(hsp_criteria_search_cert.group(2))
+            if num_pass_subjects_cert is None:
+                # Couldn't detect
+                output['criteria_passed'] = 'UNSURE'
+                output['remarks'] += 'Unable to detect if passed HSP Criteria, requires human intervention. '
+            elif num_pass_subjects_cert >= 1:
+                output['criteria_passed'] = 'PASSED'
+            else:
+                output['criteria_passed'] = 'FAILED' 
+            return output
+    else:
+        # The person likely mistakenly submitted a result slip
+        output['remarks'] += 'Result Slip was submitted instead of the proper N-Level certificate. '
+        # Check using a separate method for the result slip
+        hsp_criteria_pattern_slip = r'(graded\s*5\s*or\sbetter\:\s*(\W+)){e<=3}'
+        hsp_criteria_search_slip = regex.search(hsp_criteria_pattern_slip, first_page_merged_text)
+        num_pass_subjects_slip = word2num(hsp_criteria_search_slip.group(2))
+        if num_pass_subjects_slip is None:
+            # Couldn't detect
+            output['criteria_passed'] = 'UNSURE'
+            output['remarks'] += 'Unable to detect if passed HSP Criteria, requires human intervention. '
+        elif num_pass_subjects_slip >= 1:
+            output['criteria_passed'] = 'PASSED'
+        else:
+            output['criteria_passed'] = 'FAILED' 
+        return output
+        
+# ---------------------------------------------------------------------------- #
+#                          Analyze O-Level Certificate                         #
+# ---------------------------------------------------------------------------- #
---
+++ churn.py
@@ -0,0 +1,90 @@
+import os
+import regex
+import analysis
+from paddleocr import PaddleOCR
+
+# ---------------------------------------------------------------------------- #
+#                               Global variables                               #
+# ---------------------------------------------------------------------------- #
+
+GLOBAL_INPUT_DIR = 'data/input/'
+GLOBAL_OCR = PaddleOCR(use_angle_cls=True, lang='en', show_log=False)
+
+input_path = os.path.join(GLOBAL_INPUT_DIR, os.listdir(GLOBAL_INPUT_DIR)[0])
+
+def churn(input_file):
+    # Reject input file if it is not a PDF
+    if os.path.splitext(input_file)[-1] != '.pdf':
+        raise Exception('Input file is not a PDF.')
+    
+    # OCR the entire PDF
+    full_ocr_result = GLOBAL_OCR.ocr(input_file, cls=True)
+
+    # Consider the first page of the PDF to classify the general type of document.
+    # The general classification will be used to do a type-specific analysis which will give the desired output.
+    first_page_texts = [line[1][0] for line in full_ocr_result[0]]
+    first_page_merged_text = ' '.join(first_page_texts).lower()
+    category_counts = {}
+    categories = {
+        'nlvl' : r'normal\s*\(\w+\)\s*level', # for n-level
+        'olvl' : r'ordinary\s*level', # for o-level
+        'ite' : r'national\s*ite\s*certificate\sin', # for NITEC or Higher NTIEC
+        'poly' : r'polytechnic', # for poly
+        'ted' : r'technical\s*engineer\s*diploma', # for TED
+        'alvl' : r'advanced\s*level', # for a-level
+        'ib' : r'baccalaureate', # for IB
+        'nush' : r'nus\s*high' # for NUS High
+    }
+    for category in categories:
+        pattern = '(' + categories[category] + '){e<=1}'
+        category_counts[category] = len(regex.findall(pattern, first_page_merged_text))
+    max_count_category = max(category_counts, key=category_counts.get)
+    if max_count_category == 'psle':
+        print('psle')
+    elif max_count_category == 'nlvl':
+        return analysis.nlvl(full_ocr_result, first_page_merged_text)
+    elif max_count_category == 'olvl':
+        return analysis.olvl(full_ocr_result, first_page_merged_text)
+    elif max_count_category == 'ite':
+        # Further differentiate between NITEC and H.NITEC
+        if len(regex.findall(r'(higher\s*national\s*ite\s*certificate\sin){e<=1}', first_page_merged_text)):
+            # its a Higher NITEC
+            print('higher_nitec')
+        else:
+            print('nitec')
+    elif max_count_category == 'poly':
+        # Further differentiate between the 5 local polys
+        polys = {
+            'rp': r'republic',
+            'np': r'ngee\s*ann',
+            'nyp': r'nanyang',
+            'tp': r'temasek',
+            'sp': r'singapore'
+        }
+        poly_counts = {}
+        for poly in polys:
+            poly_pattern = '(' + polys[poly] + '){e<=1}'
+            poly_counts[poly] = len(regex.findall(poly_pattern, first_page_merged_text))
+        max_count_poly = max(poly_counts, key=poly_counts.get)
+        if max_count_poly == 'rp':
+            print('rp')
+        elif max_count_poly == 'np':
+            print('np')
+        elif max_count_poly == 'nyp':
+            print('nyp')
+        elif max_count_poly == 'tp':
+            print('tp')
+        elif max_count_poly == 'sp':
+            print('sp')
+    elif max_count_category == 'ted':
+        print('ted')
+    elif max_count_category == 'alvl':
+        print('alvl')
+    elif max_count_category == 'ib':
+        print('ib')
+    elif max_count_category == 'nush':
+        print('nush')
+
+output = churn(input_path)
+
+print(output)
