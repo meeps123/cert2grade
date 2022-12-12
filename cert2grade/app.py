2d9ecb37f0e05ce627e3c859626202d039d8ef4b
meeps123 <22002891+meeps123@users.noreply.github.com>
2022-12-12 16:36:48 +0800

    better flask directory org

--- cert2grade/app.py
+++ cert2grade/app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
 app = Flask('cert2grade')
 
-@app.route('/')
-def init():
-    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
