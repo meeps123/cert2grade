cdf08b0649bacdb5b8daddb54dc5f167c34594b3
meeps123 <22002891+meeps123@users.noreply.github.com>
2022-12-12 16:30:36 +0800

    prep for flask reorg

---
+++ cert2grade/app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask('cert2grade')
+
+@app.route('/')
+def init():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
