01232a40b5c59c6bd28faa0bb054649e338f6ec4
meeps123 <22002891+meeps123@users.noreply.github.com>
2022-12-15 11:19:28 +0800

    db schema

--- cert2grade/schema.sql
+++ cert2grade/schema.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS requests;
+DROP TABLE IF EXISTS files;
 
 CREATE TABLE user (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -10,7 +12,7 @@ CREATE TABLE user (
 CREATE TABLE requests (
     request_id INTEGER PRIMARY KEY AUTOINCREMENT,
     code TEXT UNIQUE NOT NULL,
-    tstamp INTEGER NOT NULL,
+    timestamp INTEGER NOT NULL,
     files INTEGER NOT NULL,
     size REAL NOT NULL, 
     duration REAL NOT NULL
@@ -19,6 +21,12 @@ CREATE TABLE requests (
 CREATE TABLE files (
     file_id INTEGER PRIMARY KEY AUTOINCREMENT, 
     request_id INTEGER NOT NULL,
-      
+    filepath TEXT NOT NULL,
+    preview BLOB NOT NULL,
+    hsp TEXT NOT NULL,
+    status TEXT NOT NULL,
+    score REAL,
+    type TEXT,
+    remarks TEXT,
     FOREIGN KEY(request_id) REFERENCES requests(request_id)
 )
